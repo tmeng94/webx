cb2ef7146b195b24b1f57baf29a121a69aa7262a
Te Meng <tmeng4@jhu.edu>
2017-08-03 17:42:04 +0800

    change enc/dec

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ class WebxInput extends React.Component {
         {...this.props}
         style={styles.input}
         autoCorrect={false}
+        underlineColorAndroid='transparent'
       />
     );
   }
@@ -29,17 +30,6 @@ class WebxMultilineInput extends React.Component {
   }
 }
 
-class WebxButton extends React.Component {
-  render() {
-    return (
-      <Button
-        {...this.props}
-        onPress={() => console.log('Meow')}
-      />
-    );
-  }
-}
-
 export default class App extends React.Component {
   constructor(props){ 
     super(props);
@@ -53,21 +43,15 @@ export default class App extends React.Component {
   }
 
   encrypt(text) {
-    let enc1 = CryptoJS.AES.encrypt(text, this.state.pwd1);
-    let enc2 = CryptoJS.AES.encrypt(enc1.toString(), this.state.pwd2);
-    return 'Webx!' + CryptoJS.AES.encrypt(enc2.toString(), this.state.pwd3);
+    return 'Webx!' + CryptoJS.AES.encrypt(text, this.state.pwd1 + '!' + this.state.pwd2 + '!' + this.state.pwd3);
   }
 
   decrypt(text) {
     if (text.startsWith('Webx!')) {
       try {
-        let dec1 = CryptoJS.AES.decrypt(text.slice(5), this.state.pwd3);
-        if (!dec1) return "很抱歉，我不知道你在说什么。";
-        let dec2 = CryptoJS.AES.decrypt(dec1.toString(CryptoJS.enc.Utf8), this.state.pwd2);
-        if (!dec2) return "很抱歉，我不知道你在说什么。";
-        let dec3 = CryptoJS.AES.decrypt(dec2.toString(CryptoJS.enc.Utf8), this.state.pwd1);
-        if (!dec3) return "很抱歉，我不知道你在说什么。";
-        let res = dec3.toString(CryptoJS.enc.Utf8);
+        let dec = CryptoJS.AES.decrypt(text.slice(5), this.state.pwd1 + '!' + this.state.pwd2 + '!' + this.state.pwd3);
+        if (!dec) return "很抱歉，我不知道你在说什么。";
+        let res = dec.toString(CryptoJS.enc.Utf8);
         return res? res: "很抱歉，我不知道你在说什么。"
       } catch (e) {
         return "很抱歉，我不知道你在说什么。";
@@ -80,13 +64,13 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>你的脸怎么红了：</Text>
+        <Text>你的脸怎么红了？</Text>
         <WebxInput value={this.state.pwd1} onChangeText={(text) => {
             this.setState({
               pwd1: text,
             })
           }} />
-        <Text>怎么又黄了：</Text>
+        <Text>怎么又黄了？</Text>
         <WebxInput value={this.state.pwd2} onChangeText={(text) => {
             this.setState({
               pwd2: text,
@@ -106,7 +90,6 @@ export default class App extends React.Component {
               plainText: text,
             })       
           }} />
-        <WebxButton title='爱信不信' />
       </View>
     );
   }
